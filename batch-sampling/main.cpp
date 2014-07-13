12782b298190c1b409503724eacacdae626b0de3
Veggente <veggente@gmail.com>
2014-07-13 16:13:08 -0700

    Initial set-up for Xcode

---
+++ batch-sampling/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  batch-sampling
+//
+//  Created by Veggente on 7/13/14.
+//  Copyright (c) 2014 Xiaohan Kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

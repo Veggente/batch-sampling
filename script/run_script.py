c12c2624e29dc50073b4c12333ad7c7f65569d57
Veggente <veggente@gmail.com>
2014-08-11 15:33:33 -0700

    Updated run_script.py.

--- script/run_script.py
+++ script/run_script.py
@@ -2,21 +2,18 @@
 import os
 import math
 
-def exec_time(num_servers, batch_size, arrival_rate, arrival_prob):
-    return 3000000*arrival_prob*batch_size/arrival_rate/num_servers
-
 def main():
     num_servers = [10000]
     batch_size = 100;
-    arrival_rate = [0.3]
-    arrival_prob = 0.1
-    probe_ratio = 2
-    mit_indicator = 1
+    arrival_rate = [0.99]
+    total_time = 100000
+    probe_ratio = 1.0
+    mit_indicator = 0
+    log_indicator = 0
     for n in num_servers:
         for a in arrival_rate:
-            total_time = exec_time(n, batch_size, a, arrival_prob)
             os.system("nice time ./batch-sampling "+str(n)+" "+str(batch_size)
-                      +" "+str(a)+" "+str(arrival_prob)+" "+str(total_time)+" "
+                      +" "+str(a)+" "+str(log_indicator)+" "+str(total_time)+" "
                       +str(probe_ratio)+" "+str(mit_indicator))
 
 if __name__ == "__main__":
